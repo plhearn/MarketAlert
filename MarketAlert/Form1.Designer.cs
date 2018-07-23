dc94324f4964140250e7dbb23d993cd75473e10b
plhearn <plhearn@gmail.com>
2018-07-23 07:45:18 -0400

    initital commit

--- MarketAlert/Form1.Designer.cs
+++ MarketAlert/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAlertThreshold = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDebug = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 98);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(49, 13);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "message";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Alert when price changes by ";
+            // 
+            // txtAlertThreshold
+            // 
+            this.txtAlertThreshold.Location = new System.Drawing.Point(149, 56);
+            this.txtAlertThreshold.Name = "txtAlertThreshold";
+            this.txtAlertThreshold.Size = new System.Drawing.Size(22, 20);
+            this.txtAlertThreshold.TabIndex = 2;
+            this.txtAlertThreshold.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "percent within";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "minutes.";
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(256, 56);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(21, 20);
+            this.txtMinutes.TabIndex = 5;
+            this.txtMinutes.Text = "5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDebug
+            // 
+            this.lblDebug.AutoSize = true;
+            this.lblDebug.Location = new System.Drawing.Point(399, 29);
+            this.lblDebug.Name = "lblDebug";
+            this.lblDebug.Size = new System.Drawing.Size(37, 13);
+            this.lblDebug.TabIndex = 7;
+            this.lblDebug.Text = "debug";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDebug);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAlertThreshold);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAlertThreshold;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDebug;
     }
 }
 
--- MarketAlert/Form1.cs
+++ MarketAlert/Form1.cs
@@ -7,14 +7,171 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace MarketAlert
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+        private Timer timer1;
+        public int interval = 10000;
+        public decimal alertThreshold = 0.05m;
+        public int minutes = 5;
+        string strDebug = "";
+
+        public class BinanceCoin
+        {
+            [JsonProperty("symbol")]
+            public string symbol;
+            [JsonProperty("price")]
+            public decimal price;
+        }
+
+        public class BinanceCoinData
+        {
+            public string symbol;
+            public decimal price;
+            public DateTime tstamp;
+        }
+
+        public List<BinanceCoinData> coinData = new List<BinanceCoinData>();
+
         public Form1()
         {
             InitializeComponent();
+            lblDebug.Text = "";
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1 == null)
+            {
+                InitTimer();
+                button1.Text = "Stop";
+
+                timer1_Tick(new object(), new EventArgs());
+            }
+            else
+            {
+                timer1.Stop();
+                timer1 = null;
+                button1.Text = "Start";
+
+                //timerTrades.Stop();
+                //timerTrades = null;
+            }
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = interval; // in miliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            decimal.TryParse(txtAlertThreshold.Text, out alertThreshold);
+            int.TryParse(txtMinutes.Text, out minutes);
+
+
+            getHttpBinance();
+
+            bool found = false;
+
+            decimal curPrice = 0;
+
+            if (coinData.Count > 0)
+                curPrice = coinData[coinData.Count - 1].price;
+            
+            decimal startPrice = curPrice;
+            DateTime startTime = DateTime.Now;
+
+            for (int i=coinData.Count-1; i>=0; i--)
+            {
+                if(!found)
+                {
+                    TimeSpan t = DateTime.Now - coinData[i].tstamp;
+
+                    if (t.Minutes >= minutes || i == 0)
+                    {
+                        found = true;
+                        startPrice = coinData[i].price;
+                        startTime = coinData[i].tstamp;
+                    }
+                }
+            }
+
+
+            decimal pct = 0;
+                
+            if(startPrice > 0)
+                pct = (curPrice - startPrice) / startPrice;
+
+            lblMessage.Text = "pct change over " + minutes.ToString() + " minutes: " + (pct * 100);
+
+            /*
+            strDebug = "";
+
+            for (int i = coinData.Count - 1; i >= 0; i--)
+            {
+                strDebug += coinData[i].price.ToString() + "\n";
+            }
+
+            lblDebug.Text = strDebug;
+            */
+
+            if (pct > alertThreshold / 100m)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"beep-high.wav");
+                player.Play();
+                coinData.Clear();
+
+                strDebug += "Alert at " + DateTime.Now.ToString() + " for " + alertThreshold + " pct change within " + minutes + " minutes.\n";
+                strDebug += "  Price went from " + startPrice + " at " + startTime.ToString() + " to " + curPrice + " at " + DateTime.Now.ToString() + ".\n\n";
+            }
+
+            if (pct < -alertThreshold / 100m)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"beep-low.wav");
+                player.Play();
+                coinData.Clear();
+
+                strDebug += "Alert at " + DateTime.Now.ToString() + " for " + alertThreshold + " pct change within " + minutes + " minutes.\n";
+                strDebug += "  Price went from " + startPrice + " at " + startTime.ToString() + " to " + curPrice + " at " + DateTime.Now.ToString() + ".\n\n";
+            }
+
+            lblDebug.Text = strDebug;
+        }
+
+
+        public async void getHttpBinance()
+        {
+            string strData = await client.GetStringAsync("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
+
+            //List<BinanceCoin> coins = JsonConvert.DeserializeObject<List<BinanceCoin>>(strData);
+            BinanceCoin coin = JsonConvert.DeserializeObject<BinanceCoin>(strData);
+            
+            if (coin.symbol.Contains("BTC"))
+            {
+                BinanceCoinData bcd = new BinanceCoinData();
+                bcd.price = coin.price;
+                bcd.symbol = coin.symbol;
+                bcd.tstamp = DateTime.Now;
+
+                coinData.Add(bcd);
+            }
+
+            //keep it from growing too big
+            if(coinData.Count > 1000)
+                coinData = coinData.Skip(1).ToList();
+
         }
     }
 }
